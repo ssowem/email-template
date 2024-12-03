c3be28df5a4460eb4a319b519d8b8b7078f2a695
lsyeon0825@gmail.com <lsyeon0825@gmail.com>
2024-12-03 14:38:14 +0900

    feat: 전체메뉴 버튼 토글 (CSS 수정)

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const menuBtn = document.getElementById('menu__button');
 
-menuBtn.addEventListener("click", function() {
+menuBtn.addEventListener("click", () => {
     const sendBtn = document.getElementById('send__button');
     const sendWrap = document.getElementById('nav__send');
     const navWrap = document.getElementById('mail__nav');
 
     const navText = document.querySelectorAll('.txt');
     const menuItem = document.querySelectorAll('.menu__item');
+    const itemInner = document.querySelectorAll('.item__inner');
 
     sendBtn.classList.toggle('button__active');
     sendWrap.classList.toggle('send__active')
@@ -18,5 +19,10 @@ menuBtn.addEventListener("click", function() {
 
     menuItem.forEach(function(item){
         item.classList.toggle('item__active');
+
+    })
+
+    itemInner.forEach(function(item){
+        item.classList.toggle('inner__active')
     })
 })
